e5ec00416b4fad0e6b360fbf3d3b9ff27f17c935
masami <pea98258@gmail.com>
2024-07-06 08:35:35 +0900

    withdrawal function implemented

--- app/controllers/customer/sessions_controller.rb
+++ app/controllers/customer/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Customer::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+  before_action :reject_invalid_customer, only: [:create]
   # GET /resource/sign_in
   # def new
   #   super
@@ -18,7 +18,21 @@ class Customer::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def reject_invalid_customer
+    customer = Customer.find_by(email: params[:customer][:email])
+    return unless customer
+
+    return if customer.valid_password?(params[:customer][:password]) && customer.active_for_authentication?
+
+    alert_message = if customer.status == 'withdrawn'
+                      'You have already resigned'
+                    else
+                      'Your account is suspended'
+                    end
+    redirect_to request.referer, alert: alert_message
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
